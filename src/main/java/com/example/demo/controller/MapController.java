2b10f4c9b1261cc73b7186ac2239ed52201e005c
Student <Student@Student-PC>
2024-09-02 17:38:08 +0900

    r

--- src/main/java/com/example/demo/controller/MapController.java
+++ src/main/java/com/example/demo/controller/MapController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MapController {
 
+public class MapController {
+	
     @GetMapping("/map")
     public String map() {
         return "map";  // map.html 템플릿을 렌더링
