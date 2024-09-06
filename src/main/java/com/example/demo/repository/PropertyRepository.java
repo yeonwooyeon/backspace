ead617fb356fb1a32789cb770550b6a65fdb7282
Student <Student@Student-PC>
2024-09-06 15:58:43 +0900

    ㅇ

--- src/main/java/com/example/demo/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.List;
-
-import com.domain.Property;
-
-public interface PropertyRepository {
-	List<Property> getAllPropertyList();
-}
--- src/main/java/com/example/demo/service/PropertyService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-
-import com.domain.Property;
-
-public interface PropertyService {
-	List<Property> getAllPropertylist();
-}
