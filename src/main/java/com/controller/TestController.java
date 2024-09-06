86d213755b6ebbedf555984e30879bedc89dd2fa
yeonwooyeon <yunwoo1313@gmail.com>
2024-09-06 14:01:33 +0900

    목록 테스트

--- src/main/java/com/controller/TestController.java
+++ src/main/java/com/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.domain.Property;
+import com.example.demo.service.PropertyService;
+
+@Controller
+public class PropertyController {
+	
+	@Autowired
+	private PropertyService propertyService;
+	
+	@GetMapping("/property")
+	public String requestPropertyList(Model model) {
+		List<Property> list = propertyService.getAllPropertylist();
+		model.addAttribute("propertyList", list);
+		return "property";
+	}
+	
+}
