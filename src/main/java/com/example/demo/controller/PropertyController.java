181d51e88a2a3edb00804244c193e11479cd8c28
yeonwooyeon <yunwoo1313@gmail.com>
2024-09-06 14:20:59 +0900

    목록 테스트

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import com.domain.Property;
 import com.example.demo.service.PropertyService;
 
