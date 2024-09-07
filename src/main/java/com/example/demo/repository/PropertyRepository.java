823ccece34bd62953ebb75a419bd2f74a0d6eb8a
82103 <82103@DESKTOP-9E0KEOI>
2024-09-07 15:43:01 +0900

    커밋이 되게 해주세여

---
+++ src/main/java/com/example/demo/repository/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.domain.Property;
+
+public interface PropertyRepository {
+	List<Property> getAllPropertyList();
+}
---
+++ src/main/java/com/example/demo/service/PropertyService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.domain.Property;
+
+public interface PropertyService {
+	List<Property> getAllPropertylist();
+}
