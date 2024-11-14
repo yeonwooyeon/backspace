e6e612d29b74b077f0175278a8e8aabdce9f59c6
LeeHeeSab <gmltjq2221@naver.com>
2024-11-14 16:09:23 +0900

    css

--- src/main/java/com/vacancy/controller/GlobalControllerAdvice.java
+++ src/main/java/com/vacancy/controller/GlobalControllerAdvice.java
@@ -2,15 +2,21 @@ package com.vacancy.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.study.basicboard.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @ControllerAdvice
+@RequiredArgsConstructor
 @Component
 public class GlobalControllerAdvice {
+
+    private final UserService userService;
 
     @ModelAttribute
     public void addAuthenticationAttributes(Model model) {
@@ -23,10 +29,14 @@ public class GlobalControllerAdvice {
                 && "anonymousUser".equals(authentication.getPrincipal()));
         model.addAttribute("loggedIn", loggedIn);
 
-        // 로그인된 경우 로그인 ID 추가
-        if (loggedIn && authentication.getPrincipal() instanceof UserDetails) {
-            String loginId = ((UserDetails) authentication.getPrincipal()).getUsername();
+        // 로그인된 경우 로그인 ID와 닉네임 추가
+        if (loggedIn) {
+            String loginId = authentication.getName(); // 로그인된 사용자의 ID 가져오기
             model.addAttribute("loginId", loginId);
+
+            // UserService를 통해 닉네임 가져오기
+            String nickname = userService.myInfo(loginId).getNickname();
+            model.addAttribute("userNickname", nickname); // 닉네임 추가
         }
     }
 }
